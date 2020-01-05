295b2dd7c15ea22cdb34db4fa39f93a2c6cf1059
Yohei Higashi <yoohe0722@gmail.com>
2020-01-05 19:38:30 +0900

    comit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,12 @@
 class PostsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+    Post.create(date: post_params[:date], time: post_params[:time], title: post_params[:title], text: post_params[:text])
+  end
+
 end
